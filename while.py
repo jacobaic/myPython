3a994bc7e09b494974d1ab06ce3bb9e8e7657b19
Jacob <jacob.aic@gmail.com>
2016-02-18 19:28:50 -0800

    updated

--- while.py
+++ while.py
@@ -2,7 +2,6 @@ fruits = ['apple', 'orange', 'banana']
 myfruits = ''
 index = 0
 while (index < len(fruits)):
-	print fruits[index]
-	myfruits = myfruits + ' ' + fruits[index]
+	myfruits = myfruits + fruits[index]
 	index = index + 1
 print myfruits
