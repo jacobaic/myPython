a4d5b9d4244d7ff50b7faf4adac494917824d345
jacobaic <jacob.aic@gmail.com>
2016-02-18 19:15:48 -0800

    Added files via upload

---
+++ while.py
@@ -0,0 +1,8 @@
+fruits = ['apple', 'orange', 'banana']
+myfruits = ''
+index = 0
+while (index < len(fruits)):
+	print fruits[index]
+	myfruits = myfruits + ' ' + fruits[index]
+	index = index + 1
+print myfruits
